f9a119e62aca7a4a2acd9e5be6b58b137acad85d
Joedst <joeledstrom@outlook.com>
2023-01-30 21:49:57 +0100

    - Moved Javascript to seperate file - Simple customer form validation with options to expand

---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const firstNameEl = document.getElementById('customerFirstName');
+const lastNameEl = document.getElementById('customerLastName');
+const customerEmailEl = document.getElementById('customerEmail');
+const customerPhoneEl = document.getElementById('customerPhone');
+const formEl = document.getElementById('form');
+const customerMessageEl = document.getElementById('messageId');
+let myBtn = document.getElementById("idButton");
+
+
+myBtn.addEventListener('click', (e) => { //Grund till validering 
+
+    if (customerEmailEl.value === '' || customerEmailEl.value == null ) {
+        console.log("Button was clicked");
+        console.log("User not entering contactable information")
+        alert("Email have to be inserted so we may respond to you!");
+    } else {
+        console.log("Valid message sent");
+        myBtn.innerHTML = "<h3> Message was sent!"
+    }
+}
+)
+
+
+
